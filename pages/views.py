5b67c231fe431c278cd5126030860716b79e990b
Sohel Shaikh <shaikhsohel.011@gmail.com>
2019-01-11 16:17:30 +0530

    template added

--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1> live </h1>')
+    return render(request, 'pages/index.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
