6dcc14671b4bfa74310990c2de62142ccdf61d0a
Sohel Shaikh <shaikhsohel.011@gmail.com>
2019-01-21 18:28:12 +0530

    db

--- products/urls.py
+++ products/urls.py
@@ -6,10 +6,13 @@ urlpatterns = [
     path('search', views.search, name='search'),
     path('<int:product_id>', views.product, name='product'),
     path('category/<int:category_id>', views.cat_wise, name='cat_wise'),
-    path('ajax/add_to_cart', views.add_to_cart, name='add_to_cart'),
     path('checkout_single/<int:product_id>', views.checkout_single, name='checkout_single'),
     path('checkout/<int:user_id>', views.checkout, name='checkout'),
     path('checkout/', views.checkout_tologin, name='checkout_tologin'),
+
+    # Ajax runtime response url, from every page!
+    path('ajax/add_to_cart', views.add_to_cart, name='add_to_cart'),
+    path('checkout/ajax/delete_from_cart', views.delete_from_cart, name='delete_from_cart'),
     path('category/ajax/add_to_cart', views.add_to_cart, name='add_to_cart'),
     path('ajax/delete_from_cart', views.delete_from_cart, name='delete_from_cart'),
 ]
