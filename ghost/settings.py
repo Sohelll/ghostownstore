b9572d4c8dafcad25da2b55089d80e68d7713085
Sohel Shaikh <shaikhsohel.011@gmail.com>
2019-01-23 05:41:40 +0530

    sk

--- ghost/settings.py
+++ ghost/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 's^rv=6pt1()%8s_gxv*sfo3y-_!@=7%$5p(iljane0061bibz!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
